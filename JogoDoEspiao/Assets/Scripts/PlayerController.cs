37a835c40f3e44ef8d8cf5e98ba662a24ccbd8cd
profDouglasBarcelos <prof.douglasbarcelos@gmail.com>
2017-12-04 21:33:40 -0200

    Atualizando codigo para mover somente o player local

--- JogoDoEspiao/Assets/Scripts/PlayerController.cs
+++ JogoDoEspiao/Assets/Scripts/PlayerController.cs
@@ -1,16 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : NetworkBehaviour {
 
 	// Use this for initialization
-	void Start () {
+	void Start () 
+    {
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update () 
+    {
+        
+        if (!isLocalPlayer)
+        {
+            return;
+        }
+
 		var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
         var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
 
