64b72d659dee772ae79f487079493918c474f1c7
profDouglasBarcelos <prof.douglasbarcelos@gmail.com>
2017-12-04 22:19:51 -0200

    Adicionando cena menu principal com gameManager capturando nome e definindo lista de locais

---
+++ JogoDoEspiao/Assets/Scripts/GameManager.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static List<string> locais;
+    public string nomeJogador;
+
+    public Button btnEntrar;
+    public InputField txtNome;
+
+    void TaskOnClick()
+    {
+        nomeJogador = txtNome.text;
+        Debug.Log("You have clicked the button " + nomeJogador + ", Congrats!");
+        SceneManager.LoadScene("PrimeiraCena");
+    }
+
+    void Awake()
+    {
+        DontDestroyOnLoad(transform.gameObject);
+    }
+
+    // Use this for initialization
+    void Start ()
+    {
+        Button btn = btnEntrar.GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
+
+        locais = new List<string>()
+        {
+            "Airplane",
+            "Bank",
+            "Beach",
+            "Cathedral",
+            "Circus Tent",
+            "Corporate Party",
+            "Crusader Army",
+            "Casino",
+            "Day Spa",
+            "Embassy",
+            "Hospital",
+            "Hotel",
+            "Military Base",
+            "Movie Studio",
+            "Ocean Liner",
+            "Passenger Train",
+            "Pirate Ship",
+            "Polar Station",
+            "Police Station",
+            "Restaurant",
+            "School",
+            "Service Station",
+            "Space Station",
+            "Submarine",
+            "Supermarket",
+            "Theater",
+            "University",
+            "World War II Squad"
+        };
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
